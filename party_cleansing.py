eb22958d28552115e29250c466b0f6f74b91b223
diazcal <75254021+diazcal@users.noreply.github.com>
2021-03-18 20:31:17 +0100

    Clean script
    Moved datasets to its own module

--- party_cleansing.py
+++ party_cleansing.py
@@ -1,7 +1,16 @@
 import numpy as np
 import pandas as pd
+from datasets.regions.all import *
+
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
+pd.set_option('display.max_colwidth', -1)
+
+print(df_community_and_provice)
 
 
-provinces = ["Tarragona", "Granada"]
-df = pd.DataFrame(np.random.randn(2, 2), index=provinces, columns=["PP", "PSOE"])
-print(df.filter(like="Granada"))
+# df_party_codes = pd.read_csv("datasets/02201911_MUNI/03021911.DAT")
+# provinces = ["Tarragona", "Granada"]
+# df = pd.DataFrame(np.random.randn(2, 2), index=provinces, columns=["PP", "PSOE"])
+# print(df.filter(like="Granada"))
